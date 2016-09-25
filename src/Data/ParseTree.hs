566897f1ce0a185335813f4a99711c944ad7f5bb
YuichiroSato <sato.yu1ro@gmail.com>
2016-09-25 22:16:28 +0900

    Refactoring

--- src/Data/ParseTree.hs
+++ src/Data/ParseTree.hs
@@ -5,7 +5,27 @@ module Data.ParseTree
   , Val(..)
   ) where
 
-data ParseTree = EmptyTree | Plain Exp | Sharp Exp | Quote Exp | BackQuote Exp deriving Show
-data Exp = ValExp Val | SymbolExp Symbol | Exps [ParseTree] deriving Show
-data Symbol = Special String | Variable String deriving Show
-data Val = EmptyList | IntVal Int | DoubleVal Double | CharVal Char | StringVal String | BoolVal Bool deriving Show
+data ParseTree =
+    EmptyTree
+  | Plain Exp
+  | Sharp Exp
+  | Quote Exp
+  | BackQuote Exp
+    deriving Show
+data Exp =
+    ValExp Val
+  | SymbolExp Symbol
+  | Exps [ParseTree]
+    deriving Show
+data Symbol =
+    Special String
+  | Variable String
+    deriving Show
+data Val =
+    EmptyList
+  | IntVal Int
+  | DoubleVal Double
+  | CharVal Char
+  | StringVal String
+  | BoolVal Bool
+    deriving Show
