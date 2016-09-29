48274ebb0dfc94e83b8ac9922a5388388ef88c06
YuichiroSato <sato.yu1ro@gmail.com>
2016-09-30 01:21:04 +0900

    Add function type checker

--- src/Parser/BuildInFunctions.hs
+++ src/Parser/BuildInFunctions.hs
@@ -1,5 +1,9 @@
 module Parser.BuildInFunctions
-  ( isBuildIn ) where
+  ( isBuildIn
+  , isArithmeticFunction
+  , isComparisionFunction
+  , isListOperator
+  ) where
 
 buildInFunctions :: [String]
 buildInFunctions = arithmeticFunctions ++ comparisionFunctions ++ listOperators
@@ -13,7 +17,7 @@ arithmeticFunctions =
   , "mod"]
  
 comparisionFunctions :: [String]
-conparisionFunctions =
+comparisionFunctions =
   [ "<"
   , ">"
   , "<="
@@ -28,6 +32,14 @@ listOperators =
   , "cdr"
   , "null?"]
 
+isArithmeticFunction :: String -> Bool
+isArithmeticFunction str = elem str arithmeticFunctions
+
+isComparisionFunction :: String -> Bool
+isComparisionFunction str = elem str comparisionFunctions
+
+isListOperator :: String -> Bool
+isListOperator str = elem str listOperators
 
 isBuildIn :: String -> Bool
 isBuildIn str = elem str buildInFunctions 
