e7728315b5211842f68cce4e4be12ad6a04a5604
YuichiroSato <sato.yu1ro@gmail.com>
2016-10-06 10:44:24 +0900

    Refactoring

--- src/Data/AST.hs
+++ src/Data/AST.hs
@@ -4,6 +4,25 @@ module Data.AST
   , ValArgs(..)
   ) where
 
-data AST = NullAST | ListAST [AST] | ArithmeticAST ArithmeticArgs | ValAST ValArgs deriving Show
-data ArithmeticArgs = AdditionAST [AST] | SubtractionAST [AST] | MultiplicationAST [AST] | DivisionAST [AST] | ModuloAST [AST] deriving Show
-data ValArgs = IntAST Int | DoubleAST Double | CharVal Char | StringAST String | BoolAST Bool deriving Show
+data AST =
+    NullAST
+  | ListAST [AST]
+  | ArithmeticAST ArithmeticArgs
+  | ValAST ValArgs
+      deriving Show
+
+data ArithmeticArgs =
+    AdditionAST [AST]
+  | SubtractionAST [AST]
+  | MultiplicationAST [AST]
+  | DivisionAST [AST]
+  | ModuloAST [AST]
+      deriving Show
+
+data ValArgs =
+    IntAST Int
+  | DoubleAST Double
+  | CharVal Char
+  | StringAST String
+  | BoolAST Bool
+      deriving Show
